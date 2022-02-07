46a5f201fe89b203c527246d7bf4a69742ada7f6
jkooup <evdgenij@gmail.com>
2022-02-07 23:43:44 +0300

    Fixed codestyle and added requirements.txt and .gitignore

--- image_editors/cropper.py
+++ image_editors/cropper.py
@@ -1,7 +1,8 @@
 from PIL import Image
 
 
-def crop_transform(image: Image.Image, upper_left: tuple[int, int], lower_right: tuple[int, int]) -> Image.Image:
+def crop_transform(image: Image.Image, upper_left: tuple[int, int],
+                   lower_right: tuple[int, int]) -> Image.Image:
     """
     This function crops the image.
     :param image: image for cropping.
@@ -9,5 +10,8 @@ def crop_transform(image: Image.Image, upper_left: tuple[int, int], lower_right:
     :param lower_right: right down corner for cropping.
     :return:
     """
-    assert (upper_left[0] < lower_right[0]) and (upper_left[1] < lower_right[1])
-    return image.crop((upper_left[0], upper_left[1], lower_right[0], lower_right[1]))
+    assert (upper_left[0] < lower_right[0]) and \
+           (upper_left[1] < lower_right[1])
+    return image.crop(
+        (upper_left[0], upper_left[1], lower_right[0], lower_right[1])
+    )
