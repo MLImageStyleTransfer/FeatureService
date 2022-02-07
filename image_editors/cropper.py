037b89bd0653a666fdb26f2e84193ecde33a3ea8
jkooup <evdgenij@gmail.com>
2022-02-07 23:46:27 +0300

    Changed docstrings

--- image_editors/cropper.py
+++ image_editors/cropper.py
@@ -4,10 +4,10 @@ from PIL import Image
 def crop_transform(image: Image.Image, upper_left: tuple[int, int],
                    lower_right: tuple[int, int]) -> Image.Image:
     """
-    This function crops the image.
-    :param image: image for cropping.
-    :param upper_left: left upper corner for cropping.
-    :param lower_right: right down corner for cropping.
+    This function crops the image
+    :param image: image for cropping
+    :param upper_left: left upper corner for cropping
+    :param lower_right: right down corner for cropping
     :return:
     """
     assert (upper_left[0] < lower_right[0]) and \
--- image_editors/grayscaler.py
+++ image_editors/grayscaler.py
@@ -3,9 +3,9 @@ from PIL import Image, ImageEnhance
 
 def grayscale_transform(colour_image: Image.Image) -> Image.Image:
     """
-    This function transforms the image into gray shades.
-    :param colour_image: image for transformation.
-    :return: grayscale image.
+    This function transforms the image into gray shades
+    :param colour_image: image for transformation
+    :return: grayscale image
     """
     grayscaler: ImageEnhance.Color = ImageEnhance.Color(colour_image)
     grayscale_image = grayscaler.enhance(0)
