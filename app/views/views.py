4d3698e43efaf903d6ba5374ccb26da9edee277d
Maxim Prokudin <evdgenij@gmail.com>
2022-04-10 11:44:58 +0300

    Added descriptions for views and checking request data structure

--- app/views/views.py
+++ app/views/views.py
@@ -1,6 +1,7 @@
 import flask
-from flask import request, jsonify
+import typing as tp
 from functools import partial
+from flask import request, jsonify, Response
 
 from app import app
 from app.utils import get_port
@@ -9,64 +10,209 @@ from image_editors import brightness_transform, colorfulness_transform, contrast
 from image_editors import crop_transform, grayscale_transform
 
 
-@app.route('/')
+@app.route("/")
 def index() -> flask.Response:
+    """
+    This function draws hello-page for feature service
+    """
     main_page: str = f"""
         <h1 width="100%" align="center">
             Backend feature-service!
             <br/>
-            Started on port: {get_port(place="BACKEND")}
+            Started on port: {get_port('BACKEND')}
         </h1>
     """
     return flask.Response(main_page)
 
 
-@app.route('/grayscaler/', methods=['POST'])
+@app.route("/grayscaler/", methods=["POST"])
 def grayscale_transform_view() -> flask.Response:
-    value: str = str(request.data)[3:-2]
-    res_file: str = controller(value, grayscale_transform)
-    response: flask.Response = jsonify(res_file)
+    """
+    This function processes request for image grayscale transformation
+    Request structure:
+    {
+        "image_code": str,
+        "params": None
+    }
+    :return: flask.Response with structure:
+    {
+        "image_code": str
+    }
+    """
+    request_data: tp.Optional[tp.Any] = request.get_json()
+    response: flask.Response
+
+    if isinstance(request_data, dict) and ("image_code" in request_data.keys()):
+        image_code: str = request_data["image_code"][3:-2]
+        try:
+            result_image_code: str = controller(image_code, grayscale_transform)
+            response = jsonify(result_image_code)
+        except AssertionError:
+            response = Response(status=400)
+    else:
+        response = Response(status=400)
 
     response.headers.add("Access-Control-Allow-Origin", f"http://localhost:{get_port('FRONTEND')}")
     return response
 
 
-@app.route('/cropper/', methods=['POST'])
+@app.route("/cropper/", methods=['POST'])
 def crop_transform_view() -> flask.Response:
-    value: str = str(request.data)[3:-2]
-    res_file: str = controller(value, partial(crop_transform, upper_left=(100, 100),
-                                              lower_right=(200, 200)))
-    response: flask.Response = jsonify(res_file)
+    """
+    This function processes request for image cropping
+    Request structure:
+    {
+        "image_code": str,
+        "params": {
+            "upper_left": tuple[int, int],
+            "lower_right": tuple[int, int]
+        }
+    }
+    :return: flask.Response with structure:
+    {
+        "image_code": str
+    }
+    """
+    request_data: tp.Optional[tp.Any] = request.get_json()
+    response: flask.Response
+
+    if isinstance(request_data, dict) and ({"image_code", "params"} <= request_data.keys()) and \
+            isinstance(request_data["params"], dict) and \
+            ({"upper_left", "lower_right"} <= request_data["params"].keys()):
+        image_code: str = request_data["image_code"][3:-2]
+        try:
+            result_image_code: str = controller(
+                image_code,
+                partial(crop_transform,
+                        upper_left=request_data["params"]["upper_left"],
+                        lower_right=request_data["params"]["lower_right"]
+                )
+            )
+            response = jsonify(result_image_code)
+        except AssertionError:
+            response = Response(status=400)
+    else:
+        response = Response(status=400)
 
     response.headers.add("Access-Control-Allow-Origin", f"http://localhost:{get_port('FRONTEND')}")
     return response
 
 
-@app.route('/contrast_editor/', methods=['POST'])
+@app.route("/contrast_editor/", methods=['POST'])
 def contrast_transform_view() -> flask.Response:
-    value: str = str(request.data)[3:-2]
-    res_file: str = controller(value, partial(contrast_transform, contrast_factor=1.5))
-    response: flask.Response = jsonify(res_file)
+    """
+    This function processes request for image contrast editing
+    Request structure:
+    {
+        "image_code": str,
+        "params": {
+            "contrast_factor": float,
+        }
+    }
+    :return: flask.Response with structure:
+    {
+        "image_code": str
+    }
+    """
+    request_data: tp.Optional[tp.Any] = request.get_json()
+    response: flask.Response
+
+    if isinstance(request_data, dict) and ({"image_code", "params"} <= request_data.keys()) and \
+            isinstance(request_data["params"], dict) and \
+            ("contrast_factor" in request_data["params"]):
+        image_code: str = request_data["image_code"][3:-2]
+        try:
+            result_image_code: str = controller(
+                image_code,
+                partial(contrast_transform,
+                        contrast_factor=request_data["params"]["contrast_factor"]
+                )
+            )
+            response = jsonify(result_image_code)
+        except AssertionError:
+            response = Response(status=200)
+    else:
+        response = Response(status=200)
 
     response.headers.add("Access-Control-Allow-Origin", f"http://localhost:{get_port('FRONTEND')}")
     return response
 
 
-@app.route('/colorfulness_editor/', methods=['POST'])
+@app.route("/colorfulness_editor/", methods=['POST'])
 def colorfulness_transform_view() -> flask.Response:
-    value: str = str(request.data)[3:-2]
-    res_file: str = controller(value, partial(colorfulness_transform, color_factor=1.5))
-    response: flask.Response = jsonify(res_file)
+    """
+    This function processes request for image colorfulness editing
+    Request structure:
+    {
+        "image_code": str,
+        "params": {
+            "color_factor": tuple[int, int],
+        }
+    }
+    :return: flask.Response with structure:
+    {
+        "image_code": str
+    }
+    """
+    request_data: tp.Optional[tp.Any] = request.get_json()
+    response: flask.Response
+
+    if isinstance(request_data, dict) and ({"image_code", "params"} <= request_data.keys()) and \
+            isinstance(request_data["params"], dict) and \
+            ("color_factor" in request_data["params"].keys()):
+        image_code: str = request_data["image_code"][3:-2]
+        try:
+            result_image_code: str = controller(
+                image_code,
+                partial(colorfulness_transform,
+                        color_factor=request_data["params"]["color_factor"]
+                )
+            )
+            response = jsonify(result_image_code)
+        except AssertionError:
+            response = Response(status=200)
+    else:
+        response = Response(status=200)
 
     response.headers.add("Access-Control-Allow-Origin", f"http://localhost:{get_port('FRONTEND')}")
     return response
 
 
-@app.route('/brightness_editor/', methods=['POST'])
+@app.route("/brightness_editor/", methods=['POST'])
 def brightness_transform_view() -> flask.Response:
-    value: str = str(request.data)[3:-2]
-    res_file: str = controller(value, partial(brightness_transform, brightness_factor=1.5))
-    response: flask.Response = jsonify(res_file)
+    """
+    This function processes request for image brightness editing
+    Request structure:
+    {
+        "image_code": str,
+        "params": {
+            "brightness_factor": tuple[int, int],
+        }
+    }
+    :return: flask.Response with structure:
+    {
+        "image_code": str
+    }
+    """
+    request_data: tp.Optional[tp.Any] = request.get_json()
+    response: flask.Response
+
+    if isinstance(request_data, dict) and ({"image_code", "params"} <= request_data.keys()) and \
+            isinstance(request_data["params"], dict) and \
+            ("brightness_factor" in request_data["params"].keys()):
+        image_code: str = request_data["image_code"][3:-2]
+        try:
+            result_image_code: str = controller(
+                image_code,
+                partial(brightness_transform,
+                        color_factor=request_data["params"]["brightness_factor"]
+                )
+            )
+            response = jsonify(result_image_code)
+        except AssertionError:
+            response = Response(status=200)
+    else:
+        response = Response(status=200)
 
     response.headers.add("Access-Control-Allow-Origin", f"http://localhost:{get_port('FRONTEND')}")
     return response
